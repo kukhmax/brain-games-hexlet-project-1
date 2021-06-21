8bdc4989194d3b2632e57ea9de38b8f57131ecdb
kukhmax <kukhmax@gmail.com>
2021-06-21 19:29:19 +0200

    import prompt

---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import prompt
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
